eda22fd703283801b483efd23ab4ee5b9540a356
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-05-18 21:44:15 +0200

    Opacity on btn with delay UIView.animate X DispatchQueue.main.asyncAfter

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
       let label = (sender.titleLabel?.text!)!
       playSound(soundLabel: label) // (sender.currentTitle!)
+//      reduce sender opacity to half
+      sender.alpha = 0.5
+      print("Start")
+      
+//      DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {} execute with delay 0.2
+          UIView.animate(withDuration: 0.2) {
+              sender.alpha = 1.0
+              print("End")
+          }
+
     }
 
   func playSound(soundLabel: String) {
