5ad622de6086c42e3bef5e13ee9f30be958a950d
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-05-18 20:53:47 +0200

    Xylophone Function parameter AVAudioPlayer

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+      let label = (sender.titleLabel?.text!)!
+      playSound(soundLabel: label) // (sender.currentTitle!)
+    }
 
-    
-
+  func playSound(soundLabel: String) {
+        let url = Bundle.main.url(forResource: soundLabel, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
-
